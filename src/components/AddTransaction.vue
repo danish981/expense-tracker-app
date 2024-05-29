f51617fd6962f6c88e3f4292d6a9789377660ea5
danish <danishmehmood981@gmail.com>
2024-05-29 18:12:58 +0500

    made some fixes and type in the method names

--- src/components/AddTransaction.vue
+++ src/components/AddTransaction.vue
@@ -3,18 +3,18 @@
   <form id="form" @submit.prevent="onSubmit">
     <div class="form-control">
       <label for="text">Text</label>
-      <input type="text" id="text" v-model="text" placeholder="Enter text..." />
+      <input type="text" id="text" v-model="text" placeholder="Enter text..."/>
     </div>
     <div class="form-control">
       <label for="amount"
-        >Amount <br />
+      >Amount <br/>
         (negative - expense, positive - income)</label
       >
       <input
-        type="text"
+        type="number"
         id="amount"
         v-model="amount"
-        placeholder="Enter amount..."
+        placeholder="Write text"
       />
     </div>
     <button class="btn">Add transaction</button>
@@ -23,11 +23,11 @@
 
 
 <script setup>
-import { ref } from "vue";
-import { useToast } from "vue-toastification";
+import {ref} from "vue";
+import {useToast} from "vue-toastification";
 
 const text = ref("");
-const amount = ref("");
+const amount = ref(0);
 
 const emit = defineEmits(["transactionSubmitted"]);
 
@@ -41,12 +41,13 @@ const onSubmit = () => {
 
   const transactionData = {
     text: text.value,
-    amount: parseFloat(amount.value).toFixed(2),
+    amount: amount.value,
   };
 
   emit("transactionSubmitted", transactionData);
 
   text.value = "";
-  amount.value = "";
+  amount.value = 0;
+
 };
 </script>
--- src/components/IncomeExpenses.vue
+++ src/components/IncomeExpenses.vue
@@ -2,15 +2,31 @@
   <div class="inc-exp-container">
     <div>
       <h4>Income</h4>
-      <p id="money-plus" class="money plus">+$ {{ income }} </p>
+      <p id="money-plus" class="money plus">
+        +$ {{ income }}
+      </p>
     </div>
     <div>
       <h4>Expense</h4>
-      <p id="money-minus" class="money minus">-$ {{ expense }} </p>
+      <p id="money-minus" class="money minus">
+        -$ {{ expense }}
+      </p>
     </div>
   </div>
 </template>
 
 <script setup>
-  defineProps(['income', 'expense']);
+import { defineProps } from "vue";
+
+const props = defineProps({
+  income: {
+    type: Number,
+    required: true,
+  },
+  expense: {
+    type: Number,
+    required: true,
+  },
+});
+
 </script>
